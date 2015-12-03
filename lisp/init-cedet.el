fba5cae5f43196a146a5d69750071d0c458cd244
tangjuncarl <tangjuncarl@sina.com>
2015-12-03 09:55:30 +0800

    init

--- lisp/init-cedet.el
+++ lisp/init-cedet.el
@@ -7,7 +7,7 @@
 (add-hook 'c-mode-common-hook 'my:add-semantic-to-autocomplete)
 
 ;; ede-mode 打开方便添加自定义的include类内容
-(global-ede-mode 1)
+(global-ede-mode t)
 (ede-cpp-root-project "my project" :file "~/Works/test/hello_emacs/demos/my_program/src/main.cpp" :include-path '("/../my_inc"))
 
 (global-semantic-idle-scheduler-mode 2)
