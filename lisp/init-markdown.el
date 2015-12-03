b100b828133388ddb3b2803e682eb65d4c478521
tangjuncarl <tangjuncarl@sina.com>
2015-12-03 13:13:16 +0800

    init

--- lisp/init-markdown.el
+++
@@ -1,8 +0,0 @@
-(require-package 'markdown-mode)
-
-(after-load 'whitespace-cleanup-mode
-  (push 'markdown-mode whitespace-cleanup-mode-ignore-modes))
-
-(add-auto-mode 'markdown-mode "\\.\\(text\\|markdown\\|md\\)\\'")
-
-(provide 'init-markdown)
--- lisp/init-themes.el
+++
@@ -1,7 +0,0 @@
-(load-theme 'tango-dark t)
-
-;; Setting English Font
-(set-face-attribute
- 'default nil :font "monaco 13")
-
-(provide 'init-themes)
