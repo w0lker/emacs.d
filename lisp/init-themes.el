76629402b66e12068e81ff0fccead8d5a7d82f2c
tangjuncarl <tangjuncarl@sina.com>
2015-12-07 12:45:36 +0800

    暂时完美版，添加了mode-line美化

--- lisp/init-themes.el
+++ lisp/init-themes.el
@@ -2,4 +2,10 @@
 (setq molokai-theme-kit t)
 (load-theme 'molokai t)
 
+;; 美化mode-line
+(require-package 'powerline)
+(require-package 'smart-mode-line)
+(setq sml/no-confirm-load-theme t)
+(sml/setup)
+
 (provide 'init-themes)
