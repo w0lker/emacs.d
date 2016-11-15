ecc0337812ad890458884869aab7918b89ab90bb
w0lker <w0lker.tg@gmail.com>
2016-11-15 13:16:50 +0800

    完善输入法逻辑

--- lisp/init-input-method.el
+++ lisp/init-input-method.el
@@ -5,56 +5,53 @@
 (require-package 'chinese-pyim)
 (require 'chinese-pyim)
 
-;; 输入一段拼音然后将他们转换为中文
-(global-set-key (kbd "M-j") 'pyim-convert-code-at-point)
-
-(defun my/input-method/init-pyim-configs ()
-  "初始化pyim的配置."
-  (setq default-input-method "chinese-pyim")
-  ;; 存储位置
-  (setq pyim-directory (concat user-emacs-directory
-                               (file-name-as-directory my-temp-dir)
-                               "pyim"))
-  (setq pyim-dcache-directory (concat user-emacs-directory
-                                      (file-name-as-directory my-temp-dir)
-                                      (file-name-as-directory "pyim")
-                                      "dcache"))
-
-  ;; 自动切换英文
-  (setq-default pyim-english-input-switch-functions
-        '(pyim-probe-dynamic-english
-          pyim-probe-isearch-mode
-          pyim-probe-program-mode
-          pyim-probe-evil-normal-mode
-          pyim-probe-org-structure-template))
-
-  ;; 自动切换全角和半角标点
-  (setq-default pyim-punctuation-half-width-functions
-        '(pyim-probe-punctuation-line-beginning
-          pyim-probe-punctuation-after-punctuation))
-
-  ;; 在mode-line中显示名称
-  (setq-default pyim-title "中")
-
-  ;; 每页显示词条数目
-  (setq pyim-page-length 6)
-  ;; 选框绘制实现
-  (setq pyim-page-tooltip nil)
-  ;; 单行选框
-  (setq pyim-page-style 'one-line)
-  ;; 加快反应速度，减少backends
-  (setq pyim-backends '(dcache-personal dcache-common pinyin-chars)))
-
-;; 启动时加载字典
-(add-hook 'emacs-startup-hook
-          #'(lambda ()
-              (pyim-restart-1 t)
-              (my/input-method/init-pyim-configs)))
-
 ;; 词库
 (require-package 'chinese-pyim-greatdict)
 (require 'chinese-pyim-greatdict)
 (chinese-pyim-greatdict-enable)
+
+;; 启动时加载字典
+(add-hook 'emacs-startup-hook
+          #'(lambda () (pyim-restart-1 t)))
+
+;; 输入一段拼音然后将他们转换为中文
+(global-set-key (kbd "M-j") 'pyim-convert-code-at-point)
+
+;; 设置默认输入法
+(setq default-input-method "chinese-pyim")
+
+;; 存储位置
+(setq pyim-directory (concat user-emacs-directory
+                             (file-name-as-directory my-temp-dir)
+                             "pyim"))
+(setq pyim-dcache-directory (concat user-emacs-directory
+                                    (file-name-as-directory my-temp-dir)
+                                    (file-name-as-directory "pyim")
+                                    "dcache"))
+
+;; 在mode-line中显示名称
+(setq pyim-title "中")
+;; 每页显示词条数目
+(setq pyim-page-length 6)
+;; 选框绘制实现
+(setq pyim-page-tooltip nil)
+;; 单行选框
+(setq pyim-page-style 'one-line)
+;; 加快反应速度，减少backends
+(setq pyim-backends '(dcache-personal dcache-common pinyin-chars pinyin-shortcode))
+
+;; 英文自动切换
+(setq-default pyim-english-input-switch-functions
+	      '(pyim-probe-dynamic-english
+		pyim-probe-isearch-mode
+		pyim-probe-program-mode
+		pyim-probe-evil-normal-mode
+		pyim-probe-org-structure-template))
+
+;; 全角和半角标点自动切换
+(setq-default pyim-punctuation-half-width-functions
+	      '(pyim-probe-punctuation-line-beginning
+		pyim-probe-punctuation-after-punctuation))
 
 (provide 'init-input-method)
 ;;;  init-input-method.el ends here
--- lisp/init-java.el
+++ lisp/init-java.el
@@ -1,7 +1,6 @@
-;;; package -- Java配置
+;;; package -- Java 配置
 ;;; Commentary:
 ;;; Code:
-
 
 (provide 'init-java)
 ;;;  init-java.el ends here
