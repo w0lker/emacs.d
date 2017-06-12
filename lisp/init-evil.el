01fbaae2f2cf3d9356141bc6db0b2b52a009ef09
w0lker <w0lker.tg@gmail.com>
2017-06-12 11:38:39 +0800

    修改为默认使用evil

--- lisp/init-evil.el
+++ lisp/init-evil.el
@@ -2,11 +2,18 @@
 ;;; Commentary:
 ;;; Code:
 
-(when (not window-system)
-  (config-after-fetch-require 'evil
-    (evil-mode t)
-    (setq evil-default-state 'normal)
-    )
+(config-after-fetch-require 'evil
+  (evil-mode t)
+  (setq evil-default-state 'normal
+	evil-default-cursor '("#98f5ff" box)
+	evil-normal-state-cursor '("#98f5ff" box)
+	evil-insert-state-cursor '("#98f5ff" (bar . 2))
+	evil-visual-state-cursor '("#98f5ff" box)
+	evil-replace-state-cursor '("#cd5c5c" box)
+	evil-operator-state-cursor '("#98f5ff" box)
+	evil-motion-state-cursor '("#98f5ff" box)
+	evil-emacs-state-cursor '("#adfa2f" (bar . 2))
+	)
   )
 
 (provide 'init-evil)
