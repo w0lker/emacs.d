d512b4252ea676c9384a20e484faea3fbc1000a1
w0lker <w0lker.tg@gmail.com>
2017-07-18 22:42:23 +0800

    设置prompt颜色

--- lisp/init-shell.el
+++ lisp/init-shell.el
@@ -8,7 +8,6 @@
 
 (with-eval-after-load 'shell
   (config-add-hook 'shell-mode-hook
-    ;; 设置 buffer local 级别的提示符配置
     (setq-local comint-prompt-read-only t)
     (setq-local comint-input-autoexpand t)
     
@@ -18,30 +17,33 @@
       (let ((comint-buffer-maximum-size 0))
 	(comint-truncate-buffer))
       )
-    ;; 清屏操作按键键映射为 `C-c l
     (local-unset-key (kbd "C-c l"))
     (local-set-key (kbd "C-c l") 'shell/clear-screen)
 
-    (defun shell/xterm-color-config ()
+    (config-after-fetch-require 'xterm-color
       "配置 xterm 颜色."
-      (config-after-fetch-require 'xterm-color
-	(progn (add-hook 'comint-preoutput-filter-functions 'xterm-color-filter)
-	       (setq comint-output-filter-functions (remove 'ansi-color-process-output comint-output-filter-functions)))
-	(setq compilation-environment '("TERM=xterm-256color"))
-	(add-hook 'compilation-start-hook
-		  (lambda (proc)
-		    (when (eq (process-filter proc) 'compilation-filter)
-		      (set-process-filter
-		       proc
-		       (lambda (proc string)
-			 (funcall 'compilation-filter proc
-				  (xterm-color-filter string)))))))
-	)
+      (add-hook 'comint-preoutput-filter-functions 'xterm-color-filter)
+
+      (setq comint-output-filter-functions (remove 'ansi-color-process-output comint-output-filter-functions)
+	    compilation-environment '("TERM=xterm-256color"))
+
+      (add-hook 'compilation-start-hook (lambda (proc)
+					  (when (eq (process-filter proc) 'compilation-filter)
+					    (set-process-filter proc (lambda (proc string)
+								       (funcall 'compilation-filter
+										proc
+										(xterm-color-filter string))
+								       ))
+					    )))
       )
     )
 
-  (config-after-fetch-require 'bash-completion
-    (add-hook 'shell-dynamic-complete-functions 'bash-completion-dynamic-complete))
+  (config-add-hook 'shell-dynamic-complete-functions
+    "添加bash补全."
+    (config-after-fetch-require 'bash-completion
+      (bash-completion-dynamic-complete)
+      )
+    )
   )
 
 (provide 'init-shell)
